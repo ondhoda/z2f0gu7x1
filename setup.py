2529c1dd2e95d41ff8c151aae9e3f4809a905622
ondhoda <ondhoda@outlook.com>
2024-11-10 15:29:48 +0000

    pip package

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='math_quiz',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    include_package_data=True,
+    description='math quiz',
+    author='ondhoda',
+    author_email='ondhoda@outlook.com',
+    url='https://github.com/ondhoda/z2f0gu7x1',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
